6b19756fa93974dc74e364d17bae9ea60bc9b1e6
emcpoh <kaplienkosergei@gmail.com>
2024-07-31 18:33:00 +0300

    Ex5: Парсинг JSON

---
+++ Ex5.py
@@ -0,0 +1,21 @@
+import json
+
+json_text = """
+{
+    "messages": [
+        {
+            "message": "This is the first message",
+            "timestamp": "2021-06-04 16:40:53"
+        },
+        {
+            "message": "And this is a second message",
+            "timestamp": "2021-06-04 16:41:01"
+        }
+    ]
+}
+"""
+
+json_obj = json.loads(json_text)
+
+second_message = json_obj["messages"][1]["message"]
+print(second_message)
